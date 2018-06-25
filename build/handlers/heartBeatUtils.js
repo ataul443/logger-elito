b2f524c9679c260f5950a62d215b4c19e0698882
ataul443 <ataul.one@gmail.com>
2018-06-25 22:13:13 +0530

    gitignore: typo fixed

--- build/handlers/heartBeatUtils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function heartBeatRegister(req, res) {
-    res.send({ status: "OK", service: "UP" });
-}
-exports.heartBeatRegister = heartBeatRegister;
--- build/lib/redisClent.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Redis = require("ioredis");
-exports.redis = new Redis(process.env.REDIS_URL);
